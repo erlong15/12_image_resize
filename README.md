c075ce358c54924ad010b1a4d8dcbbede1f43b28
Alex <lucky_is@mail.ru>
2017-10-11 21:26:44 +0300

    first version

--- README.md
+++ README.md
@@ -1,6 +1,49 @@
 # Image Resizer
 
-[TODO. There will be project description]
+It is a tool for an image resize. 
+
+# How to use
+
+You shoul use python 3.4 or upper
+
+Install requirements
+```
+pip install -r requrements.txt
+```  
+ You can set up width, height or scale in percents of source size.
+ If you set up scale  width and height will be ignored.
+ If you don't set up scale and width or height both, a source file measure will be used.
+
+```
+python3 image_resize.py -h
+usage: image_resize.py [-h] -i INPUT [-x WIDTH] [-y HEIGHT] [-s SCALE] -o
+                       OUTPUT
+
+Image resize tool.
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -i INPUT, --input INPUT
+                        source image
+  -x WIDTH, --width WIDTH
+                        width of result file
+  -y HEIGHT, --height HEIGHT
+                        height of result file
+  -s SCALE, --scale SCALE
+                        scale of result file in percents of source size
+  -o OUTPUT, --output OUTPUT
+                        result file name
+
+```
+
+# Example of using
+
+Create an test.png in current folder with size 100x100
+```
+python3 image_resize.py -i /home/lucky/ssdtest-slat.png -o ./test.png -x 100 -y 100
+```
+
+
 
 # Project Goals
 
--- image_resize.py
+++ image_resize.py
@@ -1,8 +1,39 @@
+import argparse
+from PIL import Image
 
 
-def resize_image(path_to_original, path_to_result):
-    pass
+def get_new_size(scale, width, height, current_width, current_height):
+    if scale:
+        result_width = round(current_width * scale / 100)
+        result_height = round(current_height * scale / 100)
+    else:
+        result_width = width if width else current_width
+        result_height = height if height else current_height
+    return result_width, result_height
+
+
+def resize_image(source_img, result_img, scale, width, height):
+    img = Image.open(source_img)
+    new_img = img.resize(get_new_size(scale, width, height, *img.size))
+    new_img.save(result_img)
+
+
+def get_args():
+    parser = argparse.ArgumentParser(description='Image resize tool.')
+    parser.add_argument('-i', '--input', help='source image',
+                        required=True)
+    parser.add_argument('-x', '--width', help='width of result file',
+                        required=False, type=int)
+    parser.add_argument('-y', '--height', help='height of result file',
+                        required=False, type=int)
+    parser.add_argument('-s', '--scale',
+                        help='scale of result file in percents of source size',
+                        required=False, type=int)
+    parser.add_argument('-o', '--output', help='result file name',
+                        required=True)
+    return parser.parse_args()
 
 
 if __name__ == '__main__':
-    pass
+    args = get_args()
+    resize_image(args.input, args.output, args.scale, args.width, args.height)
